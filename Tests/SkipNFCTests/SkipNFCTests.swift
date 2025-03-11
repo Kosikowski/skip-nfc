3cceb9e5b86d8c2cbb9814bb0fc662b9b71476e2
Marc Prud'hommeaux <marc@skip.tools>
2025-03-11 14:22:10 -0400

    Initial commit

---
+++ Tests/SkipNFCTests/SkipNFCTests.swift
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: LGPL-3.0-only WITH LGPL-3.0-linking-exception
+
+import XCTest
+import OSLog
+import Foundation
+@testable import SkipNFC
+
+let logger: Logger = Logger(subsystem: "SkipNFC", category: "Tests")
+
+@available(macOS 13, *)
+final class SkipNFCTests: XCTestCase {
+
+    func testSkipNFC() throws {
+        logger.log("running testSkipNFC")
+        XCTAssertEqual(1 + 2, 3, "basic test")
+    }
+
+    func testDecodeType() throws {
+        // load the TestData.json file from the Resources folder and decode it into a struct
+        let resourceURL: URL = try XCTUnwrap(Bundle.module.url(forResource: "TestData", withExtension: "json"))
+        let testData = try JSONDecoder().decode(TestData.self, from: Data(contentsOf: resourceURL))
+        XCTAssertEqual("SkipNFC", testData.testModuleName)
+    }
+
+}
+
+struct TestData : Codable, Hashable {
+    var testModuleName: String
+}
